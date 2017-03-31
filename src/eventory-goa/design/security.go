49b35389eb06d2021b38f2d6fddbf3f95e3b40c1
tikasan <satak47cpc@gmail.com>
2017-03-31 16:42:48 +0900

    add: AuthToken

---
+++ src/eventory-goa/design/security.go
@@ -0,0 +1,11 @@
+package design
+
+import (
+	. "github.com/goadesign/goa/design"
+	. "github.com/goadesign/goa/design/apidsl"
+)
+
+var UserAuth = APIKeySecurity("key", func() {
+	Description("ユーザートークン")
+	Header("X-Authorization")
+})
