55074023eb9802a07f82b61fe2caeeef8f750568
tikasan <satak47cpc@gmail.com>
2017-04-01 22:32:16 +0900

    add: model relation

--- design/models.go
+++ design/models.go
@@ -9,26 +9,73 @@ var _ = StorageGroup("eventory", func() {
 	Description("eventory Model")
 	Store("MySQL", gorma.MySQL, func() {
 		Description("MySQLのリレーションナルデータベース")
-		Model("events", func() {
+		Model("Events", func() {
 			RendersTo(Event)
 			Description("イベント")
 			Field("id", gorma.Integer, func() {
 				PrimaryKey()
 			})
-			Field("api_type", gorma.String)
+			Field("api_type", gorma.String, func() {
+				Alias("api_type")
+			})
 			Field("identifier", gorma.String)
-			Field("url", gorma.String)
+			Field("url", gorma.String, func() {
+				Alias("url")
+			})
 			Field("limits", gorma.Integer)
 			Field("accepte", gorma.Integer)
 			Field("wait", gorma.Integer)
 			Field("address", gorma.String)
 			Field("start_at", gorma.Timestamp)
 			Field("end_at", gorma.Timestamp)
+			Field("createdAt", gorma.Timestamp)
+			Field("updated_at", gorma.Timestamp)
+			Field("deleted_at", gorma.NullableTimestamp)
+			HasMany("EventGenres", "EventGenres")
+			HasMany("UserKeepStatus", "UserKeepStatus")
+		})
+		Model("Prefs", func() {
+			Description("都道府県")
+			Field("id", gorma.Integer, func() {
+				PrimaryKey()
+			})
+			Field("name", gorma.String)
 			Field("created_at", gorma.Timestamp)
 			Field("updated_at", gorma.Timestamp)
 			Field("deleted_at", gorma.NullableTimestamp)
+			HasMany("Events", "Events")
+			HasMany("UserFollowPrefs", "UserFollowPrefs")
 		})
-		Model("event_genres", func() {
+		Model("Genres", func() {
+			RendersTo(Genre)
+			Description("ジャンル")
+			Field("id", gorma.Integer, func() {
+				PrimaryKey()
+			})
+			Field("name", gorma.String)
+			Field("keyword", gorma.String)
+			Field("created_at", gorma.Timestamp)
+			Field("updated_at", gorma.Timestamp)
+			Field("deleted_at", gorma.NullableTimestamp)
+			HasMany("EventGenres", "EventGenres")
+			HasMany("UserFollowGenres", "UserFollowGenres")
+		})
+		Model("Users", func() {
+			Description("ユーザー")
+			Field("id", gorma.Integer, func() {
+				PrimaryKey()
+			})
+			Field("name", gorma.String)
+			Field("email", gorma.String)
+			Field("created_at", gorma.Timestamp)
+			Field("updated_at", gorma.Timestamp)
+			Field("deleted_at", gorma.NullableTimestamp)
+			HasMany("UserFollowGenres", "UserFollowGenres")
+			HasMany("UserFollowPrefs", "UserFollowPrefs")
+			HasMany("UserTerminals", "UserTerminals")
+			HasMany("UserKeepStatus", "UserKeepStatus")
+		})
+		Model("EventGenres", func() {
 			RendersTo(Event)
 			Description("イベントジャンル")
 			Field("id", gorma.Integer, func() {
@@ -40,40 +87,7 @@ var _ = StorageGroup("eventory", func() {
 			Field("updated_at", gorma.Timestamp)
 			Field("deleted_at", gorma.NullableTimestamp)
 		})
-		Model("genres", func() {
-			RendersTo(Genre)
-			Description("ジャンル")
-			Field("id", gorma.Integer, func() {
-				PrimaryKey()
-			})
-			Field("name", gorma.String)
-			Field("keyword", gorma.String)
-			Field("created_at", gorma.Timestamp)
-			Field("updated_at", gorma.Timestamp)
-			Field("deleted_at", gorma.NullableTimestamp)
-		})
-		Model("prefs", func() {
-			Description("都道府県")
-			Field("id", gorma.Integer, func() {
-				PrimaryKey()
-			})
-			Field("name", gorma.String)
-			Field("created_at", gorma.Timestamp)
-			Field("updated_at", gorma.Timestamp)
-			Field("deleted_at", gorma.NullableTimestamp)
-		})
-		Model("users", func() {
-			Description("ユーザー")
-			Field("id", gorma.Integer, func() {
-				PrimaryKey()
-			})
-			Field("name", gorma.String)
-			Field("email", gorma.String)
-			Field("created_at", gorma.Timestamp)
-			Field("updated_at", gorma.Timestamp)
-			Field("deleted_at", gorma.NullableTimestamp)
-		})
-		Model("user_follow_genres", func() {
+		Model("UserFollowGenres", func() {
 			Description("ユーザーのフォロージャンル")
 			Field("id", gorma.Integer, func() {
 				PrimaryKey()
@@ -84,7 +98,7 @@ var _ = StorageGroup("eventory", func() {
 			Field("updated_at", gorma.Timestamp)
 			Field("deleted_at", gorma.NullableTimestamp)
 		})
-		Model("user_follow_prefs", func() {
+		Model("UserFollowPrefs", func() {
 			Description("ユーザーのフォロー都道府県")
 			Field("id", gorma.Integer, func() {
 				PrimaryKey()
@@ -95,7 +109,7 @@ var _ = StorageGroup("eventory", func() {
 			Field("updated_at", gorma.Timestamp)
 			Field("deleted_at", gorma.NullableTimestamp)
 		})
-		Model("user_keep_status", func() {
+		Model("UserKeepStatus", func() {
 			Description("ユーザーのキープ状態")
 			Field("id", gorma.Integer, func() {
 				PrimaryKey()
@@ -108,7 +122,7 @@ var _ = StorageGroup("eventory", func() {
 			Field("updated_at", gorma.Timestamp)
 			Field("deleted_at", gorma.NullableTimestamp)
 		})
-		Model("user_terminals", func() {
+		Model("UserTerminals", func() {
 			Description("ユーザーの端末情報")
 			Field("id", gorma.Integer, func() {
 				PrimaryKey()
